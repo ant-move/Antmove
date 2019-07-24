a85f757400ce42d8faaa01eaa2d2970b999d658a
amap-appx <amap-appx@service.autonavi.com>
2019-07-24 20:58:35 +0800

    feat(component): component support triggerEvent and scope styles

--- packages/@antmove/wx-alipay-plugin/lifeCycles/compile/compileWxss.js
+++ packages/@antmove/wx-alipay-plugin/lifeCycles/compile/compileWxss.js
@@ -1,14 +1,46 @@
 const fs = require('fs-extra');
 const { prettierCode } = require('@antmove/utils');
 const generateAppCssStyle = require('../../generate/generateApp.css.js');
+const processClassNames = require('./processClassNames');
 
-module.exports = function (fileInfo, ctx) {
+module.exports = function (fileInfo, ctx, inCompileWxml = false) {
+    if (fileInfo.hasCompiledStyle) return false;
+
+    let originFileInfo = null;
+    let classNamesObj = {};
+    if (inCompileWxml) {
+        originFileInfo = fileInfo;
+
+        /**
+        * 在 compileWxml 中调用
+        */
+        fileInfo.parent && fileInfo.parent.children.forEach(el => {
+            if (el.extname === '.wxss') {
+                fileInfo = el;
+            }
+        });
+
+        fileInfo.hasCompiledStyle = true;
+
+        classNamesObj = processClassNames(originFileInfo);
+    }
+
+
     fileInfo.dist = fileInfo.dist.replace(/\.wxss/, '.acss');
     let cssContent = fs.readFileSync(fileInfo.path, 'utf8') || '';
     cssContent = cssContent.replace(/\.wxss"/g, '.acss";').replace(/\.wxss'/g, '.acss\';');
 
     if (fileInfo.deep === 0 || fileInfo.filename === 'app.wxss') {
         cssContent = generateAppCssStyle(cssContent, ctx.output);
+    }
+
+    if (Object.keys(classNamesObj).length) {
+        Object.keys(classNamesObj)
+            .forEach(function (className) {
+                cssContent = cssContent.replace(new RegExp(className, 'g'), function () {
+                    return classNamesObj[className].value;
+                });
+            });
     }
 
     cssContent = prettierCode(cssContent, 'scss');
