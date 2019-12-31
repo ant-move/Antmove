a04f5a4b259e936ced8c94bc7da1287794aa5605
amap-appx <amap-appx@service.autonavi.com>
2019-12-31 15:13:47 +0800

    feat(docs): update docs and fix wx-alipay bug

---
+++ packages/@antmove/utils/src/babel/sharePath.js
@@ -0,0 +1,15 @@
+module.exports = function (...p) {
+    return {
+        visitor: {
+            ObjectExpression (path) {
+                let base = path.node.properties[0];
+                if (base && base.key.name === "onShareAppMessage" && base && base.body.body[0]) {
+                    let share = base.body.body[0].argument.properties
+                    share.forEach(element => {
+                        if (element.key.name === "path" && element.value.value.charAt(0) === "/") element.value.value = element.value.value.substring(1);
+                    });
+                }
+            },
+        }
+    };
+};
